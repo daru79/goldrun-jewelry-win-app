2b3a3b07889e5ad38eede1195794c16086cbcdd6
Dariusz Wójcicki <daru79@MacBook-Pro-Dariusz.local>
2025-12-27 18:54:24 +0100

    test version 1.0.1

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Native\Desktop\Facades\AutoUpdater;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/check-for-updates', function () {
+    AutoUpdater::checkForUpdates();
+
+    dd(AutoUpdater::checkForUpdates());
+
+    return;
+})->name('check-for-updates');
